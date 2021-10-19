492df6261f9e85c915fa4ddce058d445bf86a1df
yuntaoyang <uestcyangyuntao@gmail.com>
2021-10-19 14:32:27 -0700

    fix bug

--- gpu_pipeline.py
+++ gpu_pipeline.py
@@ -5,14 +5,14 @@
 
 # set up parameters
 
-# In[4]:
+# In[17]:
 
 
 logname = 'logfile_gpu'
 max_template_date = '2021-07-15'
 
 
-# In[5]:
+# In[18]:
 
 
 path_input = '/home/yyang18/pipeline/parallelfold/fasta/'
@@ -20,10 +20,10 @@ path_output = '/home/yyang18/pipeline/parallelfold/out/' # same output directory
 path_data = '/data/yyang18/alphafold/AlphaFold/'
 path_script = '/home/yyang18/software/ParallelFold/'
 gpu_script = 'run_alphafold.sh'
-number = 4 # the number of sequence run in the same time (gpu part)
+number = 2 # the number of sequence run in the same time (gpu part)
 
 
-# In[6]:
+# In[19]:
 
 
 import subprocess
@@ -33,13 +33,13 @@ import logging
 
 # setp2: run gpu part
 
-# In[7]:
+# In[20]:
 
 
 files = os.listdir(path_input)
 
 
-# In[8]:
+# In[21]:
 
 
 # divide files into chunks for sequences run in the same time
@@ -49,21 +49,17 @@ def divide_chunks(l, n):
 files_chunk = list(divide_chunks(files, number))
 
 
-# In[9]:
+# In[22]:
 
 
 # f is file_name
 # n is the index of the file
 def parallelfold_gpu(f,n):
     script = './'+gpu_script+' '+                 '-d'+' '+path_data+' '+                 '-o'+' '+path_output+' '+                 '-m'+' '+'model_1,model_2,model_3,model_4,model_5'+' '+                 '-f'+' '+path_input+f+' '+                 '-t'+' '+max_template_date+' '+                 '-a'+' '+str(n)
-    if n < number-1:
-        subprocess.Popen(script,shell=True,cwd=path_script,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-    else:
-        process = subprocess.Popen(script,shell=True,cwd=path_script,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-        process.communicate()
+    return script
 
 
-# In[ ]:
+# In[25]:
 
 
 logging.basicConfig(level=logging.DEBUG, 
@@ -73,10 +69,14 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 logger.info("parallelfold gpu part start!")
 for files in files_chunk:
+    commands = []
     for n,file in enumerate(files):
-        parallelfold_gpu(file,n)
+        commands.append(parallelfold_gpu(file,n))
+    procs = [subprocess.Popen(i,shell=True,cwd=path_script,stdout=subprocess.PIPE,stderr=subprocess.STDOUT) for i in commands]
+    for p in procs:
+        p.communicate()
     for file in files:
-        logging.info(file+" cpu part is done!")
+        logging.info(file+" gpu part is done!")
 
 
 # In[ ]:
